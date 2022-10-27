00fd1df9e7f8ad145ed7a125ddc7d8dd0e954ed2
0xDopamine <aminebumi1@gmail.com>
2022-10-27 11:36:05 +0100

    fixed the script

--- script.sh
+++ script.sh
@@ -1,4 +1,9 @@
 URL=$(node uploadFile.js $1)
 ID=$(node upload.js ${URL})
-sleep 7
-node download.js $ID
+CMD="node download.js $ID"
+RES=$($CMD)
+while [ "$RES" == "processing" ];
+do
+    RES=$($CMD)
+done
+echo $RES
